752ed3a007993226b17fd0652c7248bc5e89e7a8
Anushka Thagle <anushkathagle10@gmail.com>
2024-11-13 00:22:55 +0530

    app setup

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,14 @@ from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 ### This function creates 
+def load_object(file_path):
+        try:
+            with open(file_path, "rb") as file_obj:
+                return pickle.load(file_obj)
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
